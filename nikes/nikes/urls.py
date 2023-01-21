293341bb5dc6f479561385537837438d05530293
deepiksrinivasans <deepikas88606@gmail.com>
2023-01-21 08:24:56 +0000

    Success

--- nikes/nikes/urls.py
+++ nikes/nikes/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('areaofrectangle/',views.rectarea,name="areaofrectangle"),
+    path('',views.rectarea,name="areaofrectangleroot")
 ]
